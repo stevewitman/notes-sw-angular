7ceb1669a8358960445737ffe459ec21dc8ba010
Steve Witman <stevewitman@gmail.com>
2015-07-20 08:29:59 -0600

    used  to get notes from my Rails API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,21 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainCtrl', function ($scope) {
-  var noteList = [
-    {
-      'title': 'Note One Title',
-      'body': 'Here is the body for note 1.'
-    },
-    {
-      'title': 'Note Two Title',
-      'body': 'Here is the body for note 2.'
-
-    },
-    {
-      'title': 'Note Three Title',
-      'body': 'Here is the body for note 3.'
+myApp.controller('mainCtrl', function ($scope, $http) {
+  var url = 'https://notes-api-sw.herokuapp.com/notes'
+  $http.get(url).success(function(data, status, headers, config) {
+    console.debug(data);
+    $scope.notes = data;
+  }).error(function(data, status, headers, config) {
+    console.error(data, status, headers, config);
+    if(status === 404) {
+      window.alert('404 - Not Found')
+    } else {
+      window.alert('unknown error')
     }
-  ];
+  });
 
-  $scope.notes = noteList;
+  $scope.addNote = function() {
+    $scope.notes.push(angular.copy($scope.note));
+  };
+
 });
